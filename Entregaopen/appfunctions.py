fcd7c13dd6fcfc054e5bb2cfdf871595fe36f9b1
Marta Torres <marta.mta35@gmail.com>
2025-09-05 21:27:22 +0200

    paste

---
+++ Entregaopen/appfunctions.py
@@ -0,0 +1,292 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 14 12:00:26 2024
+
+@author: 34711
+"""
+import streamlit as st
+import requests
+import pandas as pd
+import plotly.express as px
+import json
+
+
+from streamlit.components.v1 import html
+
+from streamlit_lottie import st_lottie
+
+#abrimos los csv
+
+festivales_city_clean = pd.read_csv(
+    r'data\festivales_city_clean.csv', sep=',').drop_duplicates()
+
+festivales_genre_clean = pd.read_csv(
+    r'data\festivales_genre_clean.csv', sep=',').drop_duplicates()
+
+festivales_concat = pd.read_csv(
+    r'data\festivales_concat.csv', sep=',').drop_duplicates()
+
+festivales_join = pd.read_csv(
+    r'data\festivales_join.csv', sep=',').drop_duplicates()
+
+numbers_to_months = {
+    1: 'Enero', 2: 'Febrero', 3: 'Marzo', 4: 'Abril', 5: 'Mayo', 6: 'Junio',
+    7: 'Julio', 8: 'Agosto', 9: 'Septiembre', 10: 'Octubre', 11: 'Noviembre', 12: 'Diciembre'
+}
+
+with open("mapa.html", "r", encoding="utf-8") as f:
+    mapa_html = f.read()
+
+
+#Definimos una función para aplicar los estilos
+
+def apply_custom_styles(
+    font="poppins",
+    background_color="#d7effa", 
+    text_color="#000000",
+    primary_color="#5c9fd9", 
+    button_text_color="#ffffff",
+    button_active_color="#3d7aa5", 
+    button_active_text_color="#ffffff",
+    secondary_background_color="#c9e4f4",
+    accent_color="#000000",
+    link_color="#4da8d9",
+):
+    st.markdown(
+        f"""
+        <style>
+        /* Definir la fuente de la página */
+        @import url('https://fonts.googleapis.com/css2?family={font}&display=swap');
+
+        body {{
+            background-color: {background_color};
+            color: {text_color};
+            font-family: '{font}', sans-serif;
+        }}
+
+        /* Personalizar los botones */
+        .stButton>button {{
+            background-color: {primary_color};
+            color: {button_text_color};
+            border: none;
+            border-radius: 5px;
+            font-weight: bold;
+            padding: 10px;
+            transition: background-color 0.3s ease;
+        }}
+
+        /* Cambiar el color del botón cuando se hace clic o está seleccionado */
+        .stButton>button:active,
+        .stButton>button:focus {{
+            background-color: {button_active_color};  /* Cambiar el fondo del botón al hacer clic */
+            color: {button_active_text_color};  /* Cambiar el color del texto cuando el botón está activo */
+        }}
+
+        /* Personalizar el sidebar (cambia el color de fondo) */
+        .css-1d391kg {{
+            background-color: {secondary_background_color};  /* Cambiar fondo del sidebar */
+        }}
+
+        /* Estilo de la aplicación principal */
+        .stApp {{
+            background-color: {background_color};
+        }}
+
+        /* Cambiar color de los enlaces */
+        a {{
+            color: {link_color};  /* Azul verdoso */
+        }}
+
+        /* Cambiar el color de los campos de texto */
+        .stTextInput, .stTextArea, .stMarkdown {{
+            color: {accent_color};  /* Amarillo suave */
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+
+# Definimos una función para crear tarjetas personalizadas
+
+def create_event_card(event_name, event_date, event_price, event_followers):
+    tarjeta_html = f"""
+    <div style="border: 3px solid #5c9fd9; border-radius: 10px; padding: 20px; margin-bottom: 20px; background-color: #ffe8db;">
+        <h4 style="text-align: center; color: #2a5487; font-size: 16px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{event_name}</h4>
+        <p style="font-size: 16px;"><strong>Fecha:</strong> {event_date}</p>
+        <p style="font-size: 16px;"><strong>Precio:</strong> {event_price}</p>
+        <p style="font-size: 16px;"><strong>Seguidores en Wegow:</strong> {event_followers}</p>
+    </div>
+    """
+    st.markdown(tarjeta_html, unsafe_allow_html=True)
+
+
+
+# Definimos una función para abrir menus de columna
+
+def column_menu(festivales, column):
+    column_elements = festivales[column].unique()
+    
+    col1, col2 = st.columns([1, 1])
+
+    with col1:
+        radio_menu_option = st.radio(
+            "Selecciona una opción",
+            column_elements,
+            label_visibility="collapsed")
+    
+    with col2:
+        if column =='city' and len(column_elements)==11:
+            st.image("images/sunsets2.jpg", use_column_width=True)
+            
+        if column =='genre':
+            st.image("images/Concert_aesthetic.jpg",use_column_width=True)
+            
+        if column =='city' and len(column_elements)!=11:
+            st.write("\n")
+            st.write('Explora los datos por ciudad, pasando el ratón por el gráfico.')
+            
+        if column =='month_name':
+            st.write("\n")
+            st.write('Explora los datos por mes, pasando el ratón por el gráfico.')
+            
+
+    return radio_menu_option
+
+
+#Definimos una función para mostrar los eventos
+                        
+def show_events(festivales, column, selection):
+    events = festivales[festivales[column] == selection].drop_duplicates(subset=['event_names'])
+
+    cols = st.columns(3)
+    
+    for idx, row in events.iterrows():
+        event = row['event_names']
+        
+        with cols[idx % 3]:
+            if st.button(f"{event}"):
+                st.markdown("##### Detalles del evento:")
+                
+                create_event_card(
+                    event_name=row['event_names'],
+                    event_date=row['event_dates'],
+                    event_price=row['event_prices'],
+                    event_followers=row['event_followers'])
+
+
+#Aplicamos el estilo
+
+apply_custom_styles(
+    font="poppins",
+    background_color="#e0f5ff",
+    text_color="#000000",
+    primary_color="#5c9fd9",
+    button_text_color="#ffffff",
+    button_active_color="#3d7aa5",
+    button_active_text_color="#ffffff",
+    secondary_background_color="#c9e4f4",
+    accent_color="#000000",
+    link_color="#4da8d9",
+)
+
+
+#Menu de opciones lateral
+side_menu_option = st.sidebar.selectbox("Elige una opción:", ["Inicio","Festivales por ciudad", "Festivales por género","Comparar precios" ,"Salir"])
+
+if side_menu_option == "Inicio":
+    st.title("¡Encuentra tu Festival Favorito!")
+    
+    with open("images/animation.json", "r") as f:
+        animation = json.load(f)
+        
+    col_animation, col_animation2, col_animation3 = st.columns([1, 2, 1])  # Esto crea 3 columnas con proporciones 1:2:1
+
+    with col_animation2:
+
+        st_lottie(animation, width=300, height=300,speed=1, loop=True)
+    
+    st.write("Descubre y compara!")
+
+    if st.button("Mostrar mapa"):
+        html(mapa_html, height=600)
+        
+        
+if side_menu_option == "Festivales por ciudad":
+    st.title("Festivales por ciudad")
+    st.write("Selecciona una ciudad:")
+    selected_city = column_menu(festivales_concat,'city')
+    show_events(festivales_concat,'city', selected_city)
+    
+    
+if side_menu_option == "Festivales por género":
+    st.title("Festivales por género")
+    st.write("Selecciona un género:")
+    selected_genre = column_menu(festivales_genre_clean, 'genre')
+    show_events(festivales_genre_clean, 'genre', selected_genre)
+    
+
+if side_menu_option == "Comparar precios":
+    st.title("Comparar precios")
+    selected_option = st.selectbox("¿Cómo quieres comparar?", ['Comparar por mes','Comparar por ciudad']) 
+    
+    if selected_option =='Comparar por mes':
+        festivales_join_sorted = festivales_join.sort_values('month')
+        months = festivales_join.sort_values('month')
+        festivales_join_sorted['month_name'] = festivales_join_sorted['month'].map(numbers_to_months)
+        selected_month = column_menu(festivales_join_sorted, 'month_name')
+        
+        with open(f"graphics/grafico_{selected_month}.html", "r", encoding="utf-8") as f:
+            graphic_month = f.read()
+            html(graphic_month, height=600)
+            
+        if st.button('Ver comparación general'):
+            with open("graphics/grafico_media_mes.html", "r", encoding="utf-8") as f:
+                graphic_month = f.read()
+                html(graphic_month, height=600)
+            
+    if selected_option =='Comparar por ciudad':
+        festivales_concat_graf = festivales_concat[festivales_concat.event_prices != 'No se especifica']
+        cities = festivales_concat_graf.city.unique()
+        selected_city = column_menu(festivales_concat_graf, 'city')
+        
+        with open(f"graphics/grafico_{selected_city}.html", "r", encoding="utf-8") as f:
+            graphic_month = f.read()
+            html(graphic_month, height=600)
+            
+            if st.button('Ver comparación general'):
+                with open("graphics/grafico_media_ciudad.html", "r", encoding="utf-8") as f:
+                    graphic_month = f.read()
+                    html(graphic_month, height=600)
+             
+                    
+if  side_menu_option == "Salir":
+    st.write('¡Hasta pronto!🎶')
+    with open("images/disc.json", "r") as f:
+        disc = json.load(f)
+        st_lottie(disc,speed=1, loop=True)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Entregaopen/scraper_wegow_completo.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  9 23:19:14 2024
+
+@author: 34711
+"""
+
+import pandas as pd
+import time
+import os
+
+from bs4 import BeautifulSoup
+from selenium import webdriver 
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+#Abrimos el navegador
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+browser.maximize_window()
+browser.get('https://www.wegow.com/es/')
+
+#Click en el botón de cookies
+cookies_button = (WebDriverWait(browser, 20).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR,
+    "#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"))))
+cookies_button.click()
+
+#Hacemos click en festivales añadiendo espera
+festivales_button = WebDriverWait(browser, 60).until(
+    EC.visibility_of_element_located((By.XPATH, "//div[@class='title-option' and text()='FESTIVALES']")))
+festivales_button.click() 
+
+#Definimos la columna de ciudades
+cities_column = WebDriverWait(browser, 60).until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR, "div.w-60.flex.flex-col")))
+
+#Definimos todas las opciones de la columna de ciudades
+cities = cities_column.find_elements(By.CSS_SELECTOR, "a.option")
+
+#Creamos un dataframe y listas vacias que rellenaremos
+festivales = pd.DataFrame(columns=["city", "event_names", "event_dates","event_prices", "event_followers"])
+
+city_names = []
+event_names = []
+event_dates = []
+event_prices = []
+event_followers = []
+
+#Tomamos los datos de cada festival de cada ciudad
+for i in range(len(cities)):
+    
+    city = cities[i]
+    name = city.text.split()[-1]
+    city.click()
+    
+    event_container = WebDriverWait(browser, 60).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "div.flex.flex-wrap.justify-start")))
+    events = WebDriverWait(browser, 60).until(
+    EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.event-card.main-small.m-1")))
+    
+    for event in events:
+        
+        event_name = event.find_element(By.CSS_SELECTOR, "h2.font-museo-500.text-left.event-name").text
+        
+        event_names.append(event_name)
+
+        event_date = event.find_element(By.CSS_SELECTOR, "span.event-date.font-museo-300").text
+        
+        event_dates.append(event_date)
+        
+        try:
+            event_price = event.find_element(By.CSS_SELECTOR, "span.font-museo-700.align-middle")
+            event_price = event_price.text
+            
+        except NoSuchElementException:
+            
+            event_price = None
+            
+        event_prices.append(event_price)
+
+        followers = event.find_element(By.CSS_SELECTOR, "span.followers-container").text
+        
+        event_followers.append(followers)
+        
+        city_names.append(name)
+        
+        
+    festivales_button = WebDriverWait(browser, 60).until(
+        EC.visibility_of_element_located((By.XPATH, "//div[@class='title-option' and text()='FESTIVALES']")))
+    festivales_button.click()
+   
+    cities_column = WebDriverWait(browser, 60).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "div.w-60.flex.flex-col")))
+    cities = cities_column.find_elements(By.CSS_SELECTOR, "a.option")
+    
+
+#Asignamos a cada columna del dataframe la lista
+festivales['city'] = city_names
+festivales['event_names'] = event_names
+festivales['event_dates'] = event_dates
+festivales['event_prices'] = event_prices
+festivales['event_followers'] = event_followers
+
+
+festivales.to_csv("./data/festivalesprueba_city.csv", index=False)
+
+#Vamos a hacer el mismo procedimiento con la columna de los géneros
+genre_column = WebDriverWait(browser, 60).until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR, "div.w-\[16rem\].flex.flex-col")))
+
+festivales_by_genre = pd.DataFrame(columns=["genre", "event_names", "event_dates","event_prices", "event_followers"])
+
+genres = genre_column.find_elements(By.CSS_SELECTOR, "a.option")
+
+genre_names = []
+event_names = []
+event_dates = []
+event_prices = []
+event_followers = []
+
+for i in range(len(genres)):
+    
+    genre = genres[i]
+    name = genre.text.split()[-1]
+    genre.click()
+    
+    event_container = WebDriverWait(browser, 60).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "div.flex.flex-wrap.justify-start")))
+    events = WebDriverWait(browser, 60).until(
+    EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.event-card.main-small.m-1")))
+    
+    for event in events:
+        
+        event_name = event.find_element(By.CSS_SELECTOR, "h2.font-museo-500.text-left.event-name").text
+    
+        event_names.append(event_name)
+
+        event_date = event.find_element(By.CSS_SELECTOR, "span.event-date.font-museo-300").text
+        
+        event_dates.append(event_date)
+        
+        try:
+            event_price = event.find_element(By.CSS_SELECTOR, "span.font-museo-700.align-middle")
+            event_price = event_price.text
+            
+        except NoSuchElementException:
+            
+            event_price = None
+            
+        event_prices.append(event_price)
+
+        followers = event.find_element(By.CSS_SELECTOR, "span.followers-container").text
+        
+        event_followers.append(followers)
+        
+        genre_names.append(name)
+        
+    festivales_button = WebDriverWait(browser, 60).until(
+        EC.visibility_of_element_located((By.XPATH, "//div[@class='title-option' and text()='FESTIVALES']")))
+    festivales_button.click()
+   
+    genre_column = WebDriverWait(browser, 60).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "div.w-\[16rem\].flex.flex-col")))
+    genres = genre_column.find_elements(By.CSS_SELECTOR, "a.option")
+
+
+festivales_by_genre['genre'] = genre_names
+festivales_by_genre['event_names'] = event_names
+festivales_by_genre['event_dates'] = event_dates
+festivales_by_genre['event_prices'] = event_prices
+festivales_by_genre['event_followers'] = event_followers
+
+festivales_by_genre.to_csv("./data/festivalesprueba_genero.csv", index=False)
+
+
+browser.quit()
+
